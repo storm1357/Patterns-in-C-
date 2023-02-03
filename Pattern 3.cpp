0527aa7894194b10ac7372cf608ebee984ed49ed
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-02-03 11:38:59 +0530

    Create Pattern 3.cpp

---
+++ Pattern 3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int n;
+  cout << "Enter the value till which the pattern has to be printed:" << endl;
+  cin >> n;
+  int i=1;
+  while (i<=n)
+    {
+      int sp=n-i;
+      while (sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int j=1;
+      while (j<=i)
+        {
+          cout << "*";
+          j++;
+        }
+      i++;
+      cout << endl;
+    }
+}
