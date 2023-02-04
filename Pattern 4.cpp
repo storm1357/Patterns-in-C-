bdca6802234a9a3caa08e28aba49ada74dfa59e3
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-02-04 12:25:46 +0530

    Update Pattern 4.cpp

--- Pattern 4.cpp
+++ Pattern 4.cpp
@@ -8,5 +8,19 @@ int main ()
   // **
   // *
   
-  
+  int n;
+  cout << "Enter the number of rows:" << endl;
+  cin >> n;
+  int i = 1;
+  while (i<=n)
+    {
+      int j=n-i+1;
+      while(j)
+        {
+          cout << "*";
+          j--;
+        }
+      i++;
+      cout << endl;
+    }
 }
