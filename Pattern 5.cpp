dd1de01eac6d3f94a3bc0d288aeb2dcbadb25a72
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-02-05 07:48:46 +0530

    Update Pattern 5.cpp

--- Pattern 5.cpp
+++ Pattern 5.cpp
@@ -8,5 +8,25 @@ int main()
    //  **
    //   *
    
-   
+   int n;
+  cout << "Enter the number of rows for the pattern:" << endl;
+  cin >> n;
+  int i=1;
+  while (i<=n)
+    {
+      int sp=i-1;
+      while (sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int j=n-i+1;
+      while (j)
+        {
+          cout << "*";
+          j--;
+        }
+      i++;
+      cout << endl;
+  }
 }
