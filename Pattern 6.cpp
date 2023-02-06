e36eb10b8f641d466187fe40380945a532a480cc
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-02-06 10:56:28 +0530

    Update Pattern 6.cpp

--- Pattern 6.cpp
+++ Pattern 6.cpp
@@ -3,5 +3,30 @@ using namespace std;
 
 int main()
 {
-  
+  // ---Pattern 22---
+  //   *
+  //  ***
+  // *****
+
+  int n;
+  cout << "Enter the number of rows for the pattern:" << endl;
+  cin >> n;
+  int i=1;
+  while(i<=n)
+    {
+      int sp=n-i;
+      while(sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int j=1;
+      while(j<=2*i-1)
+        {
+          cout << "*";
+          j++;
+        }
+      cout << endl;
+      i++;
+    }
 }
