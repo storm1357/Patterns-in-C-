99c28618e2ab2af3177ed100b3a328a7ee13bf3f
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-07 20:38:06 +0530

    Update Pattern 7.cpp

--- Pattern 7.cpp
+++ Pattern 7.cpp
@@ -3,5 +3,31 @@ using namespace std;
 
 int main()
 {
-  
+  // ---Pattern 7---
+  // *****
+  //  *** 
+  //   *
+
+  int n;
+  cout << "Enter the number rows for the pattern:" << endl;
+  cin >> n;
+  int i=1, m=n;
+  while (i<=n)
+    {
+      int sp=i-1;
+      while (sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int j=2*m-1;
+      while (j)
+        {
+          cout << "*";
+          j--;
+        }
+      m--;
+      i++;
+      cout << endl;
+    }
 }
