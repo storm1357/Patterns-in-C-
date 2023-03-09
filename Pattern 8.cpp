960f6dfbd605476648ba377f134593def02182df
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-09 11:12:53 +0530

    Update Pattern 8.cpp

--- Pattern 8.cpp
+++ Pattern 8.cpp
@@ -3,5 +3,30 @@ using namespace std;
 
 int main ()
 {
-  
+  // ---Pattern 8---
+  //   *  
+  //  * * 
+  // * * *
+
+  int n;
+  cout << "Enter ther number of rows for the pattern:" << endl;
+  cin >> n;
+  int i=1;
+  while (i<=n)
+    {
+      int sp=n-i;
+      while (sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int j=1;
+      while (j<=i)
+        {
+          cout <<"* ";
+          j++;
+        }
+      i++;
+      cout << endl;
+    }
 }
