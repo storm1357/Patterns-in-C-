3bf9f4dbd3a744eae16f2995737ce5b48be3fc3f
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-11 10:38:21 +0530

    Update Pattern 9.cpp

--- Pattern 9.cpp
+++ Pattern 9.cpp
@@ -3,5 +3,31 @@ using namespace std;
 
 int main ()
 {
-  
+  // ---Pattern 9---
+  // * * *
+  //  * *
+  //   *
+
+  int n;
+  cout << "Enter the number of rows for the pattern:" << endl;
+  cin >> n;
+  int i=1, m=n;
+  while (i<=n)
+    {
+      int sp=i-1;
+      while (sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int j=1;
+      while (j<=m)
+        {
+          cout << "* ";
+          j++;
+        }
+      i++;
+      m--;
+      cout << endl;
+    }
 }
