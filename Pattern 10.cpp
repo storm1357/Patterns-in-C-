4a1239d22144d1e3e5661aeffe6e54ccbc326547
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-13 19:00:40 +0530

    Update Pattern 10.cpp

--- Pattern 10.cpp
+++ Pattern 10.cpp
@@ -3,5 +3,38 @@ using namesapce std;
 
 int main ()
 {
-  
+  // ---Pattern 10---
+  // *
+  // **
+  // ***
+  // **
+  // *
+
+  int n;
+  cout << "Enter the number of columns for the pattern:" << endl;
+  cin >> n;
+  int i=1;
+  while (i<=n)
+    {
+      int j=1;
+      while(j<=i)
+        {
+          cout <<"*";
+          j++;
+        }
+      i++;
+      cout << endl;
+    }
+  int k=1;
+  while(k<=n-1)
+    {
+      int l=n-k;
+      while(l)
+        {
+          cout << "*";
+          l--;
+        }
+      cout << endl;
+      k++;
+    }  
 }
