285caad7a84c4b98237af786fd78422e0befb23c
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-15 20:14:07 +0530

    Update Pattern 12.cpp

--- Pattern 12.cpp
+++ Pattern 12.cpp
@@ -3,5 +3,38 @@ using namespace std;
 
 int main()
 {
-  
+  // ---Pattern 12---
+  // *      *
+  // **    **
+  // ***  ***
+  // ********
+
+  int n;
+  cout << " Enter the number ofrows for the pattern:" << endl;
+  cin >> n;
+  int i=1, m=n;
+  while (i<=n)
+    {
+      int j=1;
+      while(j<=i)
+        {
+          cout << "*";
+          j++;
+        }
+      int s=1;
+      while (s<=2*m-2)
+        {
+          cout << " ";
+          s++;
+        }
+      int k=1;
+      while (k<=i)
+        {
+          cout << "*";
+          k++;
+        }
+      i++;
+      m--;
+      cout << endl;
+    }
 }
