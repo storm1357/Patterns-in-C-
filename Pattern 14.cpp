5a222fa043444cbcce23c77159ad447b7bea1ee8
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-17 01:19:30 +0530

    Update Pattern 14.cpp

--- Pattern 14.cpp
+++ Pattern 14.cpp
@@ -3,5 +3,51 @@ using namespace std;
 
 int main ()
 {
-  
+  // ---Pattern 14---
+  //   *
+  //  ***
+  // *****
+  //  ***
+  //   *
+
+  int n;
+  cout << "Enter the number of rows for the pattern:" << endl;
+  cin >> n;
+  int i=1;
+  while (i<=n)
+    {
+      int sp=n-i;
+      while(sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int j=1;
+      while(j<=2*i-1)
+        {
+          cout << "*";
+          j++;
+        }
+      cout << endl;
+      i++;
+    }
+  int ij=1, m=n;
+  while (ij<=n)
+    {
+      int s=ij-1;
+      while (s)
+        {
+          cout << " ";
+          s--;
+        }
+      int ji=2*m-1;
+      while (ji)
+        {
+          cout << "*";
+          ji--;
+        }
+      m--;
+      ij++;
+      cout << endl;
+    }
 }
