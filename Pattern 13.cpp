31598001ad1607361870d4c0aced694d058a7c16
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-17 01:17:17 +0530

    Update Pattern 13.cpp

--- Pattern 13.cpp
+++ Pattern 13.cpp
@@ -3,5 +3,38 @@ using anmespace std;
 
 int main()
 {
-  
+  // ---Pattern 13---
+  // ********
+  // ***  ***
+  // **    **
+  // *      *
+
+  int n;
+  cout << "Enter the number of rows for the pattern:" << endl;
+  cin >> n;
+  int i=1, m=n;
+  while (i<=n)
+    {
+      int j=m;
+      while (j)
+        {
+          cout << "*";
+          j--;
+        }
+      int sp=i*2-2;
+      while (sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int k=m;
+      while (k)
+        {
+          cout << "*";
+          k--;
+        }
+      i++;
+      m--;
+      cout << endl;
+  }
 }
