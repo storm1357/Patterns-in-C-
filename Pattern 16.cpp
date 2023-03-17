4946965a42f60c2c70a68b60d86d2644fa797d18
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-18 02:25:16 +0530

    Update Pattern 16.cpp

--- Pattern 16.cpp
+++ Pattern 16.cpp
@@ -3,5 +3,53 @@ using namespace std;
 
 int main()
 {
-  
+  // ---Pattern 16---
+  // *****
+  //  ***
+  //   *
+  //   *
+  //  ***
+  // *****
+
+  int n;
+  cout << "Enter the number rows for the pattern:" << endl;
+  cin >> n;
+  int i=1, m=n;
+  while (i<=n)
+    {
+      int s=i-1;
+      while (s)
+        {
+          cout << " ";
+          s--;
+        }
+      int j=2*m-1;
+      while (j)
+        {
+          cout << "*";
+          j--;
+        }
+      m--;
+      
+      i++;
+      cout << endl;
+    }
+  int ij=1;
+  while(ij<=n)
+    {
+      int sp=n-ij;
+      while(sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int ji=1;
+      while(ji<=2*ij-1)
+        {
+          cout << "*";
+          ji++;
+        }
+      cout << endl;
+      ij++;
+    }
 }
