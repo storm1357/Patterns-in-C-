506c4039e252bccc8ad32e5ad3bdf23086f144a6
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-18 02:23:57 +0530

    Update Pattern 15.cpp

--- Pattern 15.cpp
+++ Pattern 15.cpp
@@ -3,5 +3,65 @@ using namespace std;
 
 int main()
 {
-  
+  // ---Pattern 15---
+  // *    *
+  // **  **
+  // ******
+  // ******
+  // **  **
+  // *    *
+
+  int n;
+  cout << "Enter the number of rows for the pattern to be printed:" << endl;
+  cin >> n;
+  int i=1, m=n;
+  while (i<=n)
+    {
+      int j=1;
+      while(j<=i)
+        {
+          cout << "*";
+          j++;
+        }
+      int s=1;
+      while (s<=2*m-2)
+        {
+          cout << " ";
+          s++;
+        }
+      int k=1;
+      while (k<=i)
+        {
+          cout << "*";
+          k++;
+        }
+      i++;
+      m--;
+      cout << endl;
+    }
+  int ij=1, mn=n;
+  while (ij<=n)
+    {
+      int ji=mn;
+      while (ji)
+        {
+          cout << "*";
+          ji--;
+        }
+      int sp=ij*2-2;
+      while (sp)
+        {
+          cout << " ";
+          sp--;
+        }
+      int kk=mn;
+      while (kk)
+        {
+          cout << "*";
+          kk--;
+        }
+      ij++;
+      mn--;
+      cout << endl;
+  }
 }
