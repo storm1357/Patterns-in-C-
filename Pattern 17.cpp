0db953c9fb5c3cee1d4a46bcac4e12d4dc55314b
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-03-19 12:28:33 +0530

    Update Pattern 17.cpp

--- Pattern 17.cpp
+++ Pattern 17.cpp
@@ -3,5 +3,38 @@ using namespace std;
 
 int main ()
 {
-  
+  // ---Pattern 17---
+  // *****
+  //    *
+  //   *
+  //  *
+  // *****
+
+  int n;
+  cout << "Enter the number of rows for the pattern:" << endl;
+  cin >> n;
+  int i=1;
+  while (i<=n)
+    {
+      int j=1;
+      int m=n+1;
+      while (j<=n)
+        {   
+          if(i==1||i==n)
+          {
+            cout << "*";
+          }
+          else if(i+j==m)
+          {
+            cout << "*";
+          }
+          else
+          {
+            cout << " ";
+          }
+          j++;
+        }
+      i++;
+      cout << endl;
+    }
 }
