d9c044d88cafdedad57ceade2fa460bee202ca48
storm1357 <111104708+storm1357@users.noreply.github.com>
2023-04-04 01:10:22 +0530

    Create Pattern 31.cpp

---
+++ Pattern 31.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  // ---Pattern 31---
+  // 1
+  // 23
+  // 345
+  // 4567
+
+  int n;
+  cout << "Enter the number till which the pattern has to be printed:" << endl;
+  cin >> n;
+  int i=1, a;
+  while (i<=n)
+    {
+      a=i;
+      int j=1;
+      while (j<=i)
+        {
+          cout << a << " ";
+          a++;
+          j++;
+        }
+      cout << endl;
+      i++;
+    }
+  
+  // ----OR----
+
+  //  int n;
+  // cout << "Enter the number till which the pattern has to be printed:" << endl;
+  // cin >> n;
+  // int i=1;
+  // while (i<=n)
+  //   {
+  //     int j=1;
+  //     while (j<=i)
+  //       {
+  //         cout << i+j-1 << " ";
+  //         j++;
+  //       }
+  //     cout << endl;
+  //     i++;
+  //   }
+}
